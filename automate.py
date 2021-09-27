f6765e2fcde2c39ba7060f6cdd96bc25ae4e0205
Tom <tongtianliu@gmail.com>
2021-09-27 14:46:05 -0700

    Update automate.py

---
+++ automate.py
@@ -0,0 +1,10 @@
+# import stuff
+
+# generate instructions: axis, step size, number of steps (each way?)
+
+# loop n times:
+#   take data
+#   make incremental move
+# then return and loop the other way (-incremental move)
+
+# make plot
