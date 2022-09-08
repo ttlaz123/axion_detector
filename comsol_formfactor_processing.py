d3b407ab98840b05969ff8cc06f94274d44a1e03
Taj Dyson <taj.dyson@gmail.com>
2022-09-07 17:48:06 -0700

    added script to translate comsol csvs of integrations into C vs displacement

---
+++ comsol_formfactor_processing.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+
+# Script to read form factor data from COMSOL
+# Take in a list of filenames, positions, and DoF
+# make a csv titled with the DoF, with max form factor and positions associated
+# plots the frequency associated w/ max form factor to ensure the frequency does't jump to a new resonance
+
+DoF = 'X'
+
+distances = [5, 10, 15, 20, 30] # this is for figuring out fnames
+fnames = ['aligned_form_factor.txt'] + [f'dx{distances[i]}um_form_factor.txt' for i in range(len(distances))]
+header = [6] + [1]*len(distances)
+
+Cmaxs = np.zeros(len(fnames)) # in.. you know...
+freqs = np.zeros(len(fnames)) # in GHz
+dists = np.array([0] + distances)*1e-3 # in mm
+
+interpN = 1000
+full_Cs = np.zeros((len(fnames), interpN)) # have to interpolate
+
+for i, fname in enumerate(fnames):
+
+    print(f"working on file: {fname}")
+    
+    # numpy expects j's for complex numbers...
+    with open(fname, 'rt') as f:
+        dat = f.read()
+        dat = dat.replace('i', 'j')
+    with open(fname, 'wt') as f:
+        f.write(dat)
+
+    cdat = np.genfromtxt(fname, skip_header=header[i], dtype=np.complex_)
+
+    # columns are freq, Ez, E^2, V
+    # (integrated over the whole model)
+
+    Cs = np.real(np.abs(cdat[:,1])**2 / (cdat[:,3] * cdat[:,2])) # no imag part
+
+    f = np.real(cdat[:,0].T)
+    freq2C = interp1d(f, Cs)
+    fnew = np.linspace(np.min(f), np.max(f), interpN)
+    interpd_Cs = freq2C(fnew)
+    full_Cs[i] = interpd_Cs
+    Cmaxs[i] = np.max(Cs)
+    freqs[i] = f[np.where(Cs == np.max(Cs))]
+
+    full_Cs[i][np.where(interpd_Cs == np.max(interpd_Cs))] = np.nan
+
+plt.figure()
+plt.title("Form Factor Changes with Misalignment")
+plt.ylabel("Form Factor")
+plt.xlabel(f"{DoF} Displacement (mm if linear, deg if angluar)")
+plt.plot(dists, Cmaxs, 'k')
+plt.figure()
+plt.title("Check this to see if the white line jumps peaks")
+plt.imshow(full_Cs, aspect='auto', interpolation='none', cmap="viridis_r")
+plt.colorbar()
+plt.show()
+
+
